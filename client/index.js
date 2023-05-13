3a34590845d9fd3c50e0ebb145f942bc2217db05
willowsenator <willowsenator@gmail.com>
2023-05-13 14:03:39 +0200

    Add sign Transaction and send

--- client/index.js
+++ client/index.js
@@ -1,5 +1,22 @@
 
-const {MessagingManager} = require("./MessagingManager")
+const {MessagingManager} = require("./MessagingManager");
+const {MessagingCounter} = require("./MessagingCounter");
+
 let messagingManager = new MessagingManager();
-messagingManager.getAllMessages();
-//messagingManager.writeMessage("Message: " + new Date().toDateString);
+let messagingCounter = new MessagingCounter();
+const process = async() =>{
+    await messagingCounter.getNumMessages();
+    await messagingManager.getAllMessages();
+    await messagingManager.writeMessage(new Date().toLocaleDateString() +  " " + new Date().toLocaleTimeString());
+    await new Promise(r => setTimeout(r, 2000));
+    await messagingManager.writeMessage(new Date().toLocaleDateString() +  " " + new Date().toLocaleTimeString());
+    await messagingManager.getAllMessages();
+    await messagingCounter.getNumMessages();
+    await messagingManager.markAllMessagesAsRead();
+    await new Promise(r => setTimeout(r, 2000));
+    await messagingManager.writeMessage(new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString());
+    await messagingManager.getAllMessages();
+    await messagingCounter.getNumMessages();
+}
+
+process();
