ea118dc8af12f7b93314ab4b4a9b6a8064303060
willowsenator <willowsenator@gmail.com>
2023-05-13 14:03:39 +0200

    Add sign Transaction and send

--- client/MessagingManager.js
+++ client/MessagingManager.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const Web3 = require('web3');
 const abi = JSON.parse(fs.readFileSync("./abi.json"));
-const SMART_CONTRACT_ADDRESS = "0x26704Fe8358b61dAb63ABd806d4Af57408c1f7F0";
-const ACCOUNT_ADDRESS = "" // Change with your account;
+const SMART_CONTRACT_ADDRESS = "0x9A8e4a18d7AEcC417Ae7798D7F2E059A9AB948DD";
+const PRIVATE_KEY = "YOUR_PRIVATE_KEY"; // Change with your PRIVATE_KEY
 const RPC_URL = "https://polygon-mumbai.infura.io/v3/f46dd658f6d6412c97e10ae92f624961"
-
 
 class MessagingManager{
     web3;
@@ -13,21 +12,50 @@ class MessagingManager{
     constructor(){
         this.web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
         this.messagingSmartContract = new this.web3.eth.Contract(abi, SMART_CONTRACT_ADDRESS);
+        
     }
 
-    getAllMessages(){
+    async getAllMessages(){
         this.messagingSmartContract.methods.getAllMessages().call((err, result)=>{
             console.log(result);
         })
     }
 
-    writeMessage(message){
+    async writeMessage(message){
+        const writeMessageTx = this.messagingSmartContract.methods.writeMessage(message);
 
+        // Sign Transaction with private key
+        const createTransaction = await this.web3.eth.accounts.signTransaction(
+            {
+                to: SMART_CONTRACT_ADDRESS,
+                data: writeMessageTx.encodeABI(),
+                gas: 470000
+            },
+            PRIVATE_KEY
+        );
+
+        // Send Transaction and wait for receipt
+        const createReceipt = await this.web3.eth.sendSignedTransaction(createTransaction.rawTransaction);
+        console.log(`Tx successful with hash: ${createReceipt.transactionHash}`);
     }
 
 
-    markAllMessagesAsRead(){
+    async markAllMessagesAsRead(){
+        const markAllMessagesAsReadTx = this.messagingSmartContract.methods.markAllMessagesAsRead();
 
+        // Sign Transaction with private key
+        const createTransaction = await this.web3.eth.accounts.signTransaction(
+            {
+                to: SMART_CONTRACT_ADDRESS,
+                data: markAllMessagesAsReadTx.encodeABI(),
+                gas: 470000
+            },
+            PRIVATE_KEY
+        );
+
+        // Send Transaction and wait for receipt
+        const createReceipt = await this.web3.eth.sendSignedTransaction(createTransaction.rawTransaction);
+        console.log(`Tx successful with hash: ${createReceipt.transactionHash}`);
     }
 }
 
